f8589e34b8ac01856e38e3af98a504081e64b90b
yukihane <yukihane.feather@gmail.com>
2017-04-23 21:37:58 +0900

    auto-generated files

---
+++ src/main/java/com/github/yukihane/gwt/hello_restygwt/client/Messages.java
@@ -0,0 +1,10 @@
+package com.github.yukihane.gwt.hello_restygwt.client;
+
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  @DefaultMessage("Enter your name")
+  String nameField();
+
+  @DefaultMessage("Send")
+  String sendButton();
+}
